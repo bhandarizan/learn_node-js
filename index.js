6bbfa8a3fce434453d7a1ff28ba6accd8b543bfb
rizan <bhandarizan129@gmail.com>
2026-01-22 18:10:03 +0545

    Added routes to REST API

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
-const http = require('http');
 const express = require('express');
+const users = require("./MOCK_DATA.json");
 
 const app = express();
+const PORT = 8000;
 
-app.get('/', (req, res) => {
-    res.send(`Hello, ${req.query.name || 'World'}`);
+app.get("/users", (req, res) => {
+    const html = `
+    <ul>
+      ${users.map(user => `<li>${user.first_name} </li> `).join('')}
+    </ul>
+    `;
+    res.send(html);
+  });
+
+
+//REST API endpoint 
+app.get('/api/users', (req, res) => {
+  return res.json(users);
 });
 
-app.get('/about', (req, res) => {
-    res.send('Hello, This is About Page');
+app.get('/api/users/:id', (req, res) => {
+  const id =  parseInt(req.params.id);
+  const user = users.find(user => user.id === id);
+  if (!user) {
+    return res.status(404).json({ error: "User not found" });
+  }
+  return res.json(user);
 });
 
-app.listen(8000, () => {
-    console.log('Server is listening on port 8000');
-}
-);
+app.route('/api/users/:id').get((req, res) => {
+ const id =  parseInt(req.params.id);
+  const user = users.find(user => user.id === id);
+  if (!user) {
+    return res.status(404).json({ error: "User not found" });
+  }
+  return res.json(user);
+}).post((req, res) => {
+     return res.json({ status : "POST request received" });
+}).patch((req, res) => {
+    return res.json({ status : "PATCH request received" });
+}).delete((req, res) => {
+    return res.json({ status : "DELETE request received" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
